39c37bcbf6f0211ff99ccd830bb8567001438125
dpzcieee <shairadepzzz@gmail.com>
2025-07-08 20:21:36 +0800

    Partially done code for onboarding.... most errors kay tungod empty pa ang pages nga iya padulngan

--- PetalPress/AppShell.xaml.cs
+++ PetalPress/AppShell.xaml.cs
@@ -1,10 +1,23 @@
-﻿namespace PetalPress
+﻿using PetalPress.Views.Onboarding;
+using PetalPress.Views.Auth;
+
+namespace PetalPress;
+
+public partial class AppShell : Shell
 {
-    public partial class AppShell : Shell
+    public AppShell()
     {
-        public AppShell()
-        {
-            InitializeComponent();
-        }
+        InitializeComponent();
+
+        // Register routes
+        Routing.RegisterRoute("SplashPage", typeof(SplashPage));
+        Routing.RegisterRoute("OnboardingPage", typeof(OnboardingPage));
+        Routing.RegisterRoute("GetStartedPage", typeof(GetStartedPage));
+        Routing.RegisterRoute("SignInPage", typeof(SignInPage));
+        Routing.RegisterRoute("SignUpPage", typeof(SignUpPage));
+        Routing.RegisterRoute("WelcomeMessagePage", typeof(WelcomeMessagePage));
+
+        // Navigate to splash screen at startup
+        GoToAsync("//SplashPage");
     }
 }
---
+++ PetalPress/Views/Onboarding/SplashPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.Maui.Controls;
+
+namespace PetalPress.Views.Onboarding
+{
+    public partial class SplashPage : ContentPage
+    {
+        public SplashPage()
+        {
+            InitializeComponent();
+            StartTimer();
+        }
+
+        private async void StartTimer()
+        {
+            await Task.Delay(2000); // 2 second splash screen
+            await Shell.Current.GoToAsync("//OnboardingPage");
+        }
+    }
+}
